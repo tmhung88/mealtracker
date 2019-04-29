c96755523caf4d244adef20e63d31e4a4d62dee5
Hung Tran <tmhung88@outlook.com>
2019-04-29 21:11:45 +0700

    Generated the project with start.spring.io

---
+++ webapi/src/main/java/com/mealtracker/MealTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mealtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealTrackerApplication.class, args);
+	}
+
+}
