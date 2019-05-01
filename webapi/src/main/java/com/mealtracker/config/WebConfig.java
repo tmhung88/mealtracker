6693bf3022cdebef65220f55b799c4630ea33459
Hung Tran <tmhung88@outlook.com>
2019-05-01 11:27:58 +0700

    - Fixed Jackson configuration issue - Localize validation error messages

--- webapi/src/main/java/com/mealtracker/config/WebConfig.java
+++ webapi/src/main/java/com/mealtracker/config/WebConfig.java
@@ -2,18 +2,19 @@ package com.mealtracker.config;
 
 import com.mealtracker.config.rest.CurrentUserMethodArgumentResolver;
 import com.mealtracker.exceptions.ErrorIdGenerator;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     // TODO: Extract the url into webclient.baseUrl and apply to the Local profile
@@ -27,15 +28,30 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(MAX_AGE_SECS);
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new CurrentUserMethodArgumentResolver());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(60);
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
 
     @Bean
     public ErrorIdGenerator errorIdGenerator() {
         return new ErrorIdGenerator();
-    }
-
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new CurrentUserMethodArgumentResolver());
     }
 }
