011da56d040bcf4ded7a1d0751fb037a6152dfc3
Hung Tran <tmhung88@outlook.com>
2019-05-05 14:55:37 +0700

    Allowed to load/update user settings

--- webclient/src/user/UserSettings.js
+++ webclient/src/user/UserSettings.js
@@ -1,93 +1,88 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { withPage } from '../AppPage';
+import Form from '../common/form/Form';
+import { get, put } from '../api';
 
 const styles = theme => ({
-    main: {
-        width: 'auto',
-        display: 'block', // Fix IE 11 issue.
-        marginLeft: theme.spacing.unit * 2,
-        marginRight: theme.spacing.unit * 2,
-        [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
-            width: 600,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
-    paper: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: theme.spacing.unit * 3,
-        marginBottom: theme.spacing.unit * 3,
-        padding: theme.spacing.unit * 2,
-        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-            marginTop: theme.spacing.unit * 6,
-            marginBottom: theme.spacing.unit * 6,
-            padding: theme.spacing.unit * 3,
-        },
-    },
-    avatar: {
-        margin: theme.spacing.unit,
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing.unit,
-    },
     submit: {
         marginTop: theme.spacing.unit * 3,
     },
 });
 
-function UserSettings(props) {
-    const { classes } = props;
+class UserSettings extends React.Component {
+    state = { loading: true, userSettings: { calories: 0 } }
 
-    return (
-        <main className={classes.main}>
-            <CssBaseline />
-            <Paper className={classes.paper}>
-                <Typography component="h1" variant="h5">
-                    User Settings
-                </Typography>
-                <form className={classes.form}>
+    async componentDidMount() {
+        try {
+            const response = await get(`/api/users/settings`);
+            const json = await response.json();
+            this.setState({
+                userSettings: json,
+            })
+        } finally {
+            this.setState({ loading: false });
+        }
+    }
 
-                    <FormControl margin="normal" required fullWidth>
-                        <TextField
-                            id="calories"
-                            type="number"
-                            label="Calories"
-                            className={classes.textField}
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            margin="normal"
-                        />
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        e.preventDefault();
+        this.setState({ loading: true });
+        try {
+            await put(`/api/users/settings`, this.state.userSettings);
+        } finally {
+            this.setState({ loading: false });
+        }
+    }
+    render() {
+        const { classes } = this.props;
+        const { userSettings } = this.state;
+        return (
+            <Form
+                formName="User Settings"
+                loading={this.state.loading}
+            >
+                <FormControl margin="normal" required fullWidth>
+                    <TextField
+                        id="calories"
+                        type="number"
+                        label="Calories"
+                        className={classes.textField}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        value={userSettings.calories || 0}
+                        onChange={e => {
+                            this.setState({
+                                userSettings: {
+                                    ...userSettings,
+                                    calories: Number.parseInt(e.currentTarget.value, 10),
+                                }
+                            });
+                        }}
+                        margin="normal"
+                    />
 
-                    </FormControl>
+                </FormControl>
 
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Save
-                    </Button>
-                </form>
-            </Paper>
-        </main>
-    );
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={this.handleSubmit}
+                >
+                    Save
+            </Button>
+            </Form>
+        );
+    }
 }
 
 UserSettings.propTypes = {
