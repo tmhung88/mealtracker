a0db6e619eb1b7896cbb48807f3362cd711dd728
Hung Tran <tmhung88@outlook.com>
2019-05-06 10:03:25 +0700

    Fixed the git naming issue

---
+++ webclient/src/dashboard/ListItems.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PeopleIcon from '@material-ui/icons/People';
+import FastFoodIcon from '@material-ui/icons/Fastfood';
+import { Link } from "react-router-dom";
+
+function ListItemLink(props) {
+    return <ListItem button component={Link} {...props} />;
+}
+
+export const MainListItems = ({selectedPathName}) => (
+    <div>
+        <ListItemLink to="/meals" selected={selectedPathName === "/meals"}>
+            <ListItemIcon>
+                <FastFoodIcon />
+            </ListItemIcon>
+            <ListItemText primary="Meals" />
+        </ListItemLink>
+        <ListItemLink to="/meals/all" selected={selectedPathName === "/meals/all"}>
+            <ListItemIcon>
+                <FastFoodIcon />
+            </ListItemIcon>
+            <ListItemText primary="All Meals" />
+        </ListItemLink>
+        <ListItemLink to="/users" selected={selectedPathName === "/users"}>
+            <ListItemIcon>
+                <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+        </ListItemLink>
+
+    </div>
+);
