cd213dc28f5f1aed9d1f138033a12c9b3a040e70
Hung Tran <tmhung88@outlook.com>
2019-05-07 11:44:12 +0700

    Added validation for the login page

--- webclient/src/user/Login.js
+++ webclient/src/user/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -12,12 +12,14 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { post, setToken } from '../api';
+import { post, setToken, BadRequestError } from '../api';
 import { withRouter } from "react-router-dom";
 import { Loading } from '../common/loading/Loading';
 import userSession, { Rights } from '../userSession';
 import { Link } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom'
+import ValidationForm from '../common/form/ValidationForm';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 const styles = theme => ({
   main: {
@@ -51,7 +53,7 @@ const styles = theme => ({
   },
   link: {
     marginTop: theme.spacing.unit * 1,
-    display:"inline-block",
+    display: "inline-block",
   }
 });
 
@@ -78,7 +80,14 @@ class SignIn extends React.Component {
       const json = await response.json();
       setToken(json.token);
       this.navigateToProperPage();
-    } catch{
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        console.log("BadRequestError", error.body.error.errorFields);
+        this.setState({
+          serverErrorFields: error.body.error.errorFields,
+        })
+      }
+    } finally {
       this.setState({ loading: false })
     }
 
@@ -102,38 +111,60 @@ class SignIn extends React.Component {
             Sign in
           </Typography>
           <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input id="email" name="email" autoComplete="email" autoFocus value={form.username}
-                onChange={e => this.setState({
-                  form: { ...this.state.form, username: e.currentTarget.value }
-                })} />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input name="password" type="password" id="password" autoComplete="current-password" value={form.password}
-                onChange={e => this.setState({
-                  form: { ...this.state.form, password: e.currentTarget.value }
-                })} />
-
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={this.handleSubmit}
+            <ValidationForm
+              serverErrorFields={this.state.serverErrorFields}
+              constraints={{
+                username: {
+                  email: true,
+                  presence: true,
+                },
+              }}
+              data={this.state.form}
+              onDataChange={(data) => this.setState({ form: data })}
             >
-              Sign in
-            </Button>
-            <Link className={classes.link} component={RouterLink} to="/users/register">
-              Register new User
-            </Link>
+              {({ onFieldChange, data, isValid, validationResult }) => {
+                return (<Fragment>
+                  <FormControl margin="normal" required fullWidth error={!!validationResult.username}>
+                    <InputLabel htmlFor="email">Email Address</InputLabel>
+                    <Input id="email" name="email" autoComplete="email" autoFocus
+                      value={data.username}
+                      onChange={e => onFieldChange("username", e.currentTarget.value)} />
+                    <FormHelperText>{validationResult.username}</FormHelperText>
+                  </FormControl>
+                  <FormControl margin="normal" required fullWidth error={!!validationResult.password}>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <Input name="password" type="password" id="password" autoComplete="current-password" value={data.password}
+                      onChange={e => onFieldChange("password", e.currentTarget.value)} />
+
+                  </FormControl>
+                  <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      if (!isValid()) {
+                        return;
+                      }
+
+                      this.handleSubmit(e);
+                    }}
+                  >
+                    Sign in
+                  </Button>
+                  <Link className={classes.link} component={RouterLink} to="/users/register">
+                    Register new User
+                  </Link>
+                </Fragment>)
+              }}
+            </ValidationForm>
+
           </form>
         </Paper>
       </main>
