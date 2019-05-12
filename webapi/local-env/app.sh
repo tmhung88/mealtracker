a2d23562b600700cc1402af7c713251533b265ea
Hung Tran <tmhung88@outlook.com>
2019-05-12 13:12:58 +0700

    Activate Spring profile at compile time with Maven. By default, it's local

--- webapi/local-env/app.sh
+++ webapi/local-env/app.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+#
+echo ">>>>>>>>>>>>>>>>>>> Pull MySQL docker image if it's not available locally, run unit tests & integration tests"
+./mvnw clean verify -P ci-server
+
+echo ">>>>>>>>>>>>>>>>>>> Starting up docker containers: MySQL"
 cd local-env
 docker-compose up -d
 
+echo ">>>>>>>>>>>>>>>>>>> Starting the application Meal Tracker"
 cd ..
-./mvnw clean compile spring-boot:run -Dspring-boot.run.profiles=local
+./mvnw clean compile -DskipTests spring-boot:run
+
+
--- webapi/src/integration-test/java/com/mealtracker/repositories/UserRepositoryIT.java
+++ webapi/src/integration-test/java/com/mealtracker/repositories/UserRepositoryIT.java
@@ -9,14 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.MySQLContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-
 public class UserRepositoryIT {
 
     @ClassRule
